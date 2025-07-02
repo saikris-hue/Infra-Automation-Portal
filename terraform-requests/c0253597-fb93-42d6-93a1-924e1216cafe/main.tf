c9c27cfcf2325105305bf4293bbd812c244a8698
saikris-hue <chinthasaikrishna@outlook.com>
2025-07-02 09:24:51 -0400

    Add terraform-requests/c0253597-fb93-42d6-93a1-924e1216cafe/main.tf for request c0253597-fb93-42d6-93a1-924e1216cafe

---
+++ terraform-requests/c0253597-fb93-42d6-93a1-924e1216cafe/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+
+  network_rules {
+    default_action = var.network_default_action
+  }
+
+  tags = var.tags
+}
