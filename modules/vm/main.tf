2b2b0df132fc02d46d398401e43c1348304e1715
saikris-hue <chinthasaikrishna@outlook.com>
2025-07-02 11:53:39 -0400

    Add/update modules/vm/main.tf for request b7b27e21-a0ec-4d02-9af4-9644be5b2a9f

---
+++ modules/vm/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+
+  os_disk {
+    caching              = var.os_disk_caching
+    storage_account_type = var.os_disk_storage_type
+  }
+
+  source_image_reference {
+    publisher = var.source_image_publisher
+    offer     = var.source_image_offer
+    sku       = var.source_image_sku
+    version   = var.source_image_version
+  }
+
+  network_interface_ids = var.network_interface_ids
+
+  tags = var.tags
+}
