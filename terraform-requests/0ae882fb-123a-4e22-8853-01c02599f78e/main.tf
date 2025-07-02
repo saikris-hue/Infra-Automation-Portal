eab477b720a693a401799be6971b24f28c0f2f87
saikris-hue <chinthasaikrishna@outlook.com>
2025-07-02 10:37:24 -0400

    Add terraform-requests/0ae882fb-123a-4e22-8853-01c02599f78e/main.tf for request 0ae882fb-123a-4e22-8853-01c02599f78e

---
+++ terraform-requests/0ae882fb-123a-4e22-8853-01c02599f78e/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+
+  network_rules {
+    default_action             = var.network_default_action
+    ip_rules                   = var.ip_rules
+    virtual_network_subnet_ids = var.virtual_network_subnet_ids
+  }
+
+  tags = var.tags
+}
