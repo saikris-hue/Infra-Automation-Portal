1a445789680dcc074f3918fe6673f414f63aeb0f
saikris-hue <chinthasaikrishna@outlook.com>
2025-07-02 11:53:43 -0400

    Add/update main.tf for request b7b27e21-a0ec-4d02-9af4-9644be5b2a9f

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# This file serves as the root configuration
+# Individual deployments are defined in the deployments/ directory
