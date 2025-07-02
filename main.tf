21228954abaff0fc130a25bcbd167d9e682eab72
saikris-hue <chinthasaikrishna@outlook.com>
2025-07-02 13:28:47 -0400

    Add/update main.tf for request 4090ce7a-80ca-4a12-beb5-88f48a1b0cfa

--- main.tf
+++ main.tf
@@ -43,3 +43,33 @@ output "storage_30352b7a_3851_4eea_b095_1bd3851ef707_name" {
   description = "Name of Storage Account 30352b7a-3851-4eea-b095-1bd3851ef707"
   value       = module.storage_30352b7a_3851_4eea_b095_1bd3851ef707.storage_account_name
 }
+
+# Storage Module for Request 4090ce7a-80ca-4a12-beb5-88f48a1b0cfa
+module "storage_4090ce7a_80ca_4a12_beb5_88f48a1b0cfa" {
+  source = "./modules/storage"
+  
+  storage_account_name     = var.storage_4090ce7a_80ca_4a12_beb5_88f48a1b0cfa_account_name
+  resource_group_name      = var.storage_4090ce7a_80ca_4a12_beb5_88f48a1b0cfa_resource_group_name
+  location                = var.storage_4090ce7a_80ca_4a12_beb5_88f48a1b0cfa_location
+  account_tier            = var.storage_4090ce7a_80ca_4a12_beb5_88f48a1b0cfa_account_tier
+  account_replication_type = var.storage_4090ce7a_80ca_4a12_beb5_88f48a1b0cfa_account_replication_type
+  
+  network_default_action = var.storage_4090ce7a_80ca_4a12_beb5_88f48a1b0cfa_network_default_action
+  ip_rules              = var.storage_4090ce7a_80ca_4a12_beb5_88f48a1b0cfa_ip_rules
+  virtual_network_subnet_ids = var.storage_4090ce7a_80ca_4a12_beb5_88f48a1b0cfa_virtual_network_subnet_ids
+  
+  tags = {
+    RequestId = "4090ce7a-80ca-4a12-beb5-88f48a1b0cfa"
+    ManagedBy = "InfrastructureRequestSystem"
+  }
+}
+
+output "storage_4090ce7a_80ca_4a12_beb5_88f48a1b0cfa_id" {
+  description = "ID of Storage Account 4090ce7a-80ca-4a12-beb5-88f48a1b0cfa"
+  value       = module.storage_4090ce7a_80ca_4a12_beb5_88f48a1b0cfa.storage_account_id
+}
+
+output "storage_4090ce7a_80ca_4a12_beb5_88f48a1b0cfa_name" {
+  description = "Name of Storage Account 4090ce7a-80ca-4a12-beb5-88f48a1b0cfa"
+  value       = module.storage_4090ce7a_80ca_4a12_beb5_88f48a1b0cfa.storage_account_name
+}
